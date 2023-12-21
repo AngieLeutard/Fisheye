0d7211612131fec4cf760a816a278b6d55ca4d94
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2023-12-21 18:10:05 +0100

    add changes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,40 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const description = document.createElement( 'div' );
+        description.classList.add('description_wrapper');
+
+        const cityName = document.createElement( 'span' );
+        cityName.textContent = city;
+        cityName.classList.add('city_name')
+
+        const taglineContent = document.createElement( 'span' );
+        taglineContent.textContent = tagline;
+        taglineContent.classList.add('tagline_content')
+
+        const priceValue = document.createElement( 'span' );
+        priceValue.textContent = price + "€/jour";
+        priceValue.classList.add('price_value')
+
+        description.appendChild(cityName);
+        description.appendChild(taglineContent);
+        description.appendChild(priceValue);
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(description);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price, getUserCardDOM }
 }
