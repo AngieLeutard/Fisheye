6c35110f093e1b5ded68527a60d44ec8621fcc7e
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2024-01-02 12:27:10 +0100

    added photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,97 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// recupérer l'id de l'url
+
+const url = new URL(window.location.href);
+const id = url.searchParams.get("id");
+
+// récupérer data (détails + photo + médias) cf kasa find
+
+// pour les medias il faut faire un filter 
+
+
+async function getData(id) {
+    
+    const response = await fetch("photographers.json")
+    let photographers = await response.json()
+    console.log(photographers)
+
+    return ({ 
+        photographer: photographers.photographers.find(photographer => photographer.id == id),
+        medias: photographers.media.filter(media => media.photographerId == id) 
+    })
+}
+
+getData(id).then(
+    data => {
+        console.log(data)
+        displayHeader(data.photographer)
+        displayGallery(data.medias)
+    }
+)
+
+function displayHeader(photographer) {
+    console.log(photographer)
+    const informations = document.querySelector(".photograph-header")
+
+    const informations_wrapper = document.createElement("div");
+    informations_wrapper.classList.add('infos_wrapper')
+
+    const title = document.createElement( 'h2' );
+    title.textContent = photographer.name;
+    title.classList.add('title');
+
+    const city = document.createElement( 'span' );
+    city.textContent = photographer.city + ", " + photographer.country;
+    city.classList.add('city');
+
+    const citation = document.createElement( 'span' ); 
+    citation.textContent = photographer.tagline;
+    citation.classList.add('citation');
+
+    const picture = document.createElement( 'img' );
+    const pictureSrc = `assets/photographers/${photographer.portrait}`;
+    picture.setAttribute("src", pictureSrc);
+    picture.classList.add('picture')
+
+    informations.appendChild(informations_wrapper)
+    informations.appendChild(picture);
+
+    informations_wrapper.appendChild(title);
+    informations_wrapper.appendChild(city);
+    informations_wrapper.appendChild(citation);
+
+    return(informations);
+
+
+}
+
+function displayGallery(medias) {
+    console.log(medias)
+
+    const main = document.querySelector(".photograph_description");
+
+    const gallery = document.createElement("div");
+    gallery.classList.add('gallery');
+
+    const item = document.createElement("a");
+    item.classList.add('item');
+
+    const item_img = document.createElement("img");
+    item_img.classList.add('item_img');
+
+    const item_title = document.createElement("span");
+    item_title.classList.add('item_title');
+
+    item.appendChild(item_img);
+    item.appendChild(item_title);
+
+    gallery.appendChild(item);
+
+    main.appendChild(gallery);
+
+    return(main);
+
+}
+
+
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,9 +1,14 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, tagline, price } = data;
+    const { name, portrait, city, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const pageLink = `photographer.html?id=${id}`;
 
     function getUserCardDOM() {
+
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", pageLink);
+
         const article = document.createElement( 'article' );
 
         const img = document.createElement( 'img' );
@@ -24,17 +29,19 @@ function photographerTemplate(data) {
         taglineContent.classList.add('tagline_content')
 
         const priceValue = document.createElement( 'span' );
-        priceValue.textContent = price + "€/jour";
+        priceValue.textContent = price + "€ / jour";
         priceValue.classList.add('price_value')
 
         description.appendChild(cityName);
         description.appendChild(taglineContent);
         description.appendChild(priceValue);
 
+        link.appendChild(article);
+
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(description);
-        return (article);
+        return (link);
     }
     return { name, picture, city, tagline, price, getUserCardDOM }
 }
